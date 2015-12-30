b4948f8b3e4d7e0016a2b9b51fc9e698e141914f
Mohammed Mahgoub <mmahgoub@gmail.com>
2015-12-30 14:50:30 +0300

    refactor: Removed unnecessary reportMatch() third argument from test file

--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -88,8 +88,8 @@ def testReportMatches():
     registerPlayer("Diane Grant")
     standings = playerStandings()
     [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2, 1)
-    reportMatch(id3, id4, 2)
+    reportMatch(id1, id2)
+    reportMatch(id3, id4)
     standings = playerStandings()
     for (i, n, w, m) in standings:
         if m != 1:
@@ -110,8 +110,8 @@ def testPairings():
     registerPlayer("Pinkie Pie")
     standings = playerStandings()
     [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2, 1)
-    reportMatch(id3, id4, 2)
+    reportMatch(id1, id2)
+    reportMatch(id3, id4)
     pairings = swissPairings()
     if len(pairings) != 2:
         raise ValueError(
